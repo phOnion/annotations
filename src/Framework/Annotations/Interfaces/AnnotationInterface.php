e55d68f0697ff26bb7bce4788f1e57b1ee010996
Dimitar Dimitrov <daghostman.dimitrov@gmail.com>
2016-10-01 12:55:52 +0300

    Added interface to allow type hinting annotations (possible future usage for mandatory methods)

---
+++ src/Framework/Annotations/Interfaces/AnnotationInterface.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * PHP Version 5.6.0
+ *
+ * @category Unknown Category
+ * @package  Onion\Framework\Annotations\Interfaces
+ * @author   Dimitar Dimitrov <daghostman.dd@gmail.com>
+ * @license  MIT https://opensource.org/licenses/MIT
+ * @link     https://github.com/phOnion/framework
+ */
+
+namespace Onion\Framework\Annotations\Interfaces;
+
+
+interface AnnotationInterface
+{
+
+}
